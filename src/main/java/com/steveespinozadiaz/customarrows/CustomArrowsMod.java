bb7e01cf92a511b203a7e2be0bfa91cf2b35107d
SteveEspinozaDiaz <Steve_EspinozaDiaz@student.uml.edu>
2022-07-03 20:01:35 -0400

    Refactor mod class

--- src/main/java/com/steveespinozadiaz/customarrows/CustomArrowsMod.java
+++ src/main/java/com/steveespinozadiaz/customarrows/CustomArrowsMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.steveespinozadiaz.customarrows;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -18,13 +18,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(CustomArrowsMod.MOD_ID)
+public class CustomArrowsMod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public static final String MOD_ID = "customarrows";
+
+    public CustomArrowsMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
